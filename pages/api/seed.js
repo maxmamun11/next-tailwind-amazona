eb80e7b21899ed8c744b65f8dfeae1fb0b083707
Abdullah Al mamun <abdullaalmamun100@gmail.com>
2022-08-23 15:13:48 +0800

    Seed sample Products

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,11 +1,14 @@
 import db from '..//../utils/db';
 import User from '..//../models/User';
 import data from '..//../utils/data';
+import Product from '../../models/Product';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 };
